cb2b1713eb53a50f5032ed6acf5f90b449586106
Fundor333 <fundor333@fundor333.com>
2023-05-25 18:54:22 +0200

    Start the project

---
+++ app.py
@@ -0,0 +1,42 @@
+import requests
+import click
+
+
+class MastodonSocial:
+    def __init__(self, token: str, domain: str) -> None:
+        self.token = token
+        self.domain = domain
+
+    def follow(self, account_id: str) -> None:
+        url = f"https://{self.domain}/api/v1/accounts/{account_id}/follow"
+        headers = {"Authorization": f"Bearer {self.token}"}
+        response = requests.post(url, headers=headers)
+        if response.status_code == 200:
+            print("Success")
+
+    def get_users_from_hashtags(self, hashtag: str) -> None:
+        url = f"https://{self.domain}/api/v1/timelines/tag/{hashtag}"
+        headers = {"Authorization": f"Bearer {self.token}"}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return [user["account"]["id"] for user in response.json()]
+        else:
+            raise Exception("Error")
+
+    def run(self, hashtag: str) -> None:
+        users = self.get_users_from_hashtags(hashtag)
+        for user in users:
+            self.follow(user)
+
+
+@click.command()
+@click.option('--token', help='Token for Mastodon')
+@click.option('--domain', default="mastodon.social", help='Mastodon server')
+@click.option('--hashtag', default=1, help='The hash tag to find the followers')
+def run(token, domain, hashtag):
+    ms = MastodonSocial(token=token, domain=domain)
+    ms.run(hashtag=hashtag)
+
+
+if __name__ == '__main__':
+    run()
